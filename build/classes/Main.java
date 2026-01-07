44f1acbba27f52b7d279dc340bd5d9e59a3528f3
sc23ade <sc23ade@herts.ac.uk>
2026-01-08 02:38:14 +0530

    Main.java

---
+++ build/classes/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+import view.MainFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new MainFrame();
+        });
+    }
+}
